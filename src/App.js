3f7a321ad2425a0a51c8b93eb44c0c93f49efc56
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-14 22:18:45 +0600

    onchange, onblur, onsubmit and also installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import app from "./firebase.init";
+import { getAuth } from "firebase/auth";
 
+const auth = getAuth(app);
 function App() {
-  return <div className="App"></div>;
+  const handleEmailBlur = (event) => {
+    console.log(event.target.value);
+  };
+  const handlePasswordBlur = (e) => {
+    console.log(e.target.value);
+  };
+  const handleFormSubmit = (event) => {
+    console.log("form submited");
+    event.preventDefault();
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleFormSubmit}>
+        <input onBlur={handleEmailBlur} type="email" placeholder="email" />{" "}
+        <br />
+        <input
+          onBlur={handlePasswordBlur}
+          type="password"
+          placeholder="password"
+        />{" "}
+        <br />
+        <input type="button" value="Login" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
