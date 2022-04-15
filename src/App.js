5780eb443a48ec9e90acd20044909c2632b7fac8
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-16 00:25:06 +0600

    simple register from validation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ const auth = getAuth(app);
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState("");
 
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
@@ -27,23 +29,43 @@ function App() {
       .catch((error) => {
         console.error(error);
       });
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+
+    if (!/^(?=.*[a-z])/.test(password)) {
+      setError("Please password should contain a-z");
+      return;
+    }
+
+    setValidated(true);
+    setError(" ");
   };
 
   return (
     <div>
       <div className="registration w-50 mx-auto my-5">
         <h2 className="text-primary">Please Register</h2>
-        <Form onSubmit={handleFormSubmit}>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control
               onBlur={handleEmailBlur}
               type="email"
               placeholder="Enter email"
+              required
             />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -52,11 +74,18 @@ function App() {
               onBlur={handlePasswordBlur}
               type="password"
               placeholder="Password"
+              required
             />
+
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
           </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
+          <p className="text-danger">{error}</p>
+
+          <Button onClick={handleFormSubmit} variant="primary" type="submit">
+            Register
           </Button>
         </Form>
       </div>
